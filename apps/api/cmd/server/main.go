ba41afe738ee57a5260ba89af8055625d4c13750
ASPCODES <adityawork0811@gmail.com>
2026-06-30 16:50:46 +0530

    Creating server the app is running

--- apps/api/cmd/server/main.go
+++ apps/api/cmd/server/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/health", func(c *fiber.Ctx) error {
+        return c.JSON(fiber.Map{"status": "ok"})
+    })
+
+	log.Println("Server starting at port 8080")
+	if err := app.Listen(":8080"); err != nil {
+		log.Fatal(err)
+	}
+
+}
